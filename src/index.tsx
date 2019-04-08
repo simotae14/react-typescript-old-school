8f2ffce0f30196adf5f1a4a5acbf9101009b2f5c
Simone Taeggi <startae14@gmail.com>
2019-04-08 19:19:55 +0200

    Add the main Component and Home configurations

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import { Home } from './components/Home';
+
+ReactDOM.render(<Home name="Simone" age={34} />, document.getElementById('app'));
