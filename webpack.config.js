03726546eb853c41c38da651b5e9b97560fe1a3f
Simone Taeggi <startae14@gmail.com>
2019-04-08 19:34:27 +0200

    doesn't work

--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,14 @@ module.exports = {
     },
     devtool: "source-map",
     resolve: {
-        extensions: ["", ".ts", ".tsx", ".js"]
+        extensions: ['.ts', '.tsx', '.js']
     },
     module: {
         rules: [
             {
               test: /\.tsx?$/,
-              use: 'ts-loader'
+              use: 'ts-loader',
+              exclude: /node_modules/
             }
         ]
     }
